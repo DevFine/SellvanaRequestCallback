668999c17cdbca12d7c44c5509803ee1d95085ab
enarc <enarc@atwix.com>
2014-07-20 15:50:52 +0300

    Added english translation for the product page entries

---
+++ i18n/en.php
@@ -0,0 +1,7 @@
+<?php return [
+    'Request a Callback' => '',
+    'You can leave your telephone number and we will call you shortly' => '',
+    'Name:' => '',
+    'Phone number:' => '',
+    'Submit' => ''
+];
