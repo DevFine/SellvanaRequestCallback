5cbcbdf676460274ed86d389a7d90d2689d59eb4
enarc <enarc@atwix.com>
2014-07-27 12:34:10 +0300

    Backend validation has been added

--- Frontend/Controller.php
+++ Frontend/Controller.php
@@ -12,9 +12,13 @@ class DevFine_RequestCallback_Frontend_Controller extends FCom_Frontend_Controll
         /** @var DevFine_RequestCallback_Model_Request $entity */
         $entity = $this->DevFine_RequestCallback_Model_Request;
 
+        if (false == $entity->validate($requestData, [], 'frontend')) {
+            $this->message($this->_('You must fill all required fields'), 'error');
+            return $this->BResponse->redirect($this->BRequest->referrer());
+        }
+
         try {
             $entity->submitRequest($requestData);
-
             $successMessage = $this->_(
                 sprintf('%s, thank you for contacting us. We will get back to you in few minutes',
                 $requestData['name'])
